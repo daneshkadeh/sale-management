8aea75fcb2b4690bd72c072cdd1a258f1bc5e2d4
congbangmm83 <congbangmm83@gmail.com>
2011-08-25 15:15:23 +0000

    __Revert revision 36 - why did you delete 2 file? :D Please be sure that code build successfully after your commit.

---
+++ ssm/ssm-core/src/main/java/com/hbsoft/ssm/view/object/DetailDataModel.java
@@ -0,0 +1,48 @@
+package com.hbsoft.ssm.view.object;
+
+public class DetailDataModel {
+    private String fieldName;
+    private FieldTypeEnum fieldType;
+    private boolean isEditable;
+    private boolean isEnable;
+
+    public DetailDataModel(String fieldName, FieldTypeEnum fieldType, boolean isEditable, boolean isEnable) {
+        this.fieldName = fieldName;
+        this.fieldType = fieldType;
+        this.isEditable = isEditable;
+        this.isEnable = isEnable;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public void setFieldName(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    public FieldTypeEnum getFieldType() {
+        return fieldType;
+    }
+
+    public void setFieldType(FieldTypeEnum fieldType) {
+        this.fieldType = fieldType;
+    }
+
+    public boolean isEditable() {
+        return isEditable;
+    }
+
+    public void setEditable(boolean isEditable) {
+        this.isEditable = isEditable;
+    }
+
+    public boolean isEnable() {
+        return isEnable;
+    }
+
+    public void setEnable(boolean isEnable) {
+        this.isEnable = isEnable;
+    }
+
+}
