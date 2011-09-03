f9bdedc1255b1ea2fd1262bae955aefab8c64a88
hoanglt705 <hoanglt705@b168cc76-a07e-71fc-40dc-6a1c1b62a871>
2011-09-03 16:26:12 +0000

    __add tbl_auth_principal, tbl_auth_user, tbl_auth_user_principal, tbl_auth_login

--- ssm/sql-migration/sales.sql
+++ ssm/sql-migration/sales.sql
@@ -1,21 +1,97 @@
 -- phpMyAdmin SQL Dump
--- version 3.2.0.1
+-- version 2.11.0
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 15, 2011 at 05:31 AM
--- Server version: 5.1.36
--- PHP Version: 5.3.0
+-- Generation Time: Sep 04, 2011 at 12:23 AM
+-- Server version: 5.0.45
+-- PHP Version: 5.2.4
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 --
 -- Database: `sales`
 --
-drop table if exists tbl_detail_invoice;
-drop table if exists tbl_invoice;
-drop table if exists tbl_goods;
-drop table if exists tbl_customer;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_auth_login`
+--
+
+CREATE TABLE `tbl_auth_login` (
+  `id` int(11) NOT NULL auto_increment,
+  `appName` varchar(250) NOT NULL,
+  `loginModuleClass` varchar(250) NOT NULL,
+  `controlFlag` varchar(250) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `tbl_auth_login`
+--
+
+INSERT INTO `tbl_auth_login` (`id`, `appName`, `loginModuleClass`, `controlFlag`) VALUES
+(1, 'simpleDb', 'com.hbsoft.ssm.security.SalesLoginModule', 'REQUIRED');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_auth_principal`
+--
+
+CREATE TABLE `tbl_auth_principal` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(250) NOT NULL,
+  `class` varchar(250) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `tbl_auth_principal`
+--
+
+INSERT INTO `tbl_auth_principal` (`id`, `name`, `class`) VALUES
+(1, 'Manager', 'com.hbsoft.ssm.security.SalesPrincipal');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_auth_user`
+--
+
+CREATE TABLE `tbl_auth_user` (
+  `id` int(11) NOT NULL auto_increment,
+  `username` varchar(250) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `tbl_auth_user`
+--
+
+INSERT INTO `tbl_auth_user` (`id`, `username`, `password`) VALUES
+(1, 'hoangle', '123456');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_auth_user_principal`
+--
+
+CREATE TABLE `tbl_auth_user_principal` (
+  `user_id` int(11) NOT NULL,
+  `principal_id` int(11) NOT NULL,
+  PRIMARY KEY  (`user_id`,`principal_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_auth_user_principal`
+--
+
+INSERT INTO `tbl_auth_user_principal` (`user_id`, `principal_id`) VALUES
+(1, 1);
 
 -- --------------------------------------------------------
 
@@ -23,10 +99,10 @@ drop table if exists tbl_customer;
 -- Table structure for table `tbl_customer`
 --
 
-CREATE TABLE IF NOT EXISTS `tbl_customer` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tbl_customer` (
+  `id` int(11) NOT NULL auto_increment,
   `name` varchar(250) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
@@ -40,19 +116,19 @@ CREATE TABLE IF NOT EXISTS `tbl_customer` (
 -- Table structure for table `tbl_detail_invoice`
 --
 
-CREATE TABLE IF NOT EXISTS `tbl_detail_invoice` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tbl_detail_invoice` (
+  `id` int(11) NOT NULL auto_increment,
   `goods_id` int(11) NOT NULL,
   `goods_name` varchar(250) NOT NULL,
-  `quantity` int(11) NOT NULL DEFAULT '0',
-  `price_before_tax` double NOT NULL DEFAULT '0',
-  `tax` double NOT NULL DEFAULT '0',
-  `price_after_tax` double NOT NULL DEFAULT '0',
-  `money_before_tax` double NOT NULL DEFAULT '0',
-  `money_of_tax` double NOT NULL DEFAULT '0',
-  `money_after_tax` double NOT NULL DEFAULT '0',
-  `invoice_id` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
+  `quantity` int(11) NOT NULL default '0',
+  `price_before_tax` double NOT NULL default '0',
+  `tax` double NOT NULL default '0',
+  `price_after_tax` double NOT NULL default '0',
+  `money_before_tax` double NOT NULL default '0',
+  `money_of_tax` double NOT NULL default '0',
+  `money_after_tax` double NOT NULL default '0',
+  `invoice_id` int(11) NOT NULL default '0',
+  PRIMARY KEY  (`id`),
   KEY `invoice_id` (`invoice_id`),
   KEY `goods_id` (`goods_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
@@ -68,15 +144,15 @@ CREATE TABLE IF NOT EXISTS `tbl_detail_invoice` (
 -- Table structure for table `tbl_goods`
 --
 
-CREATE TABLE IF NOT EXISTS `tbl_goods` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tbl_goods` (
+  `id` int(11) NOT NULL auto_increment,
   `name` varchar(250) NOT NULL,
-  `priceBeforeTax` double NOT NULL DEFAULT '0',
-  `tax` double NOT NULL DEFAULT '0',
-  `price_after_tax` double NOT NULL DEFAULT '0',
-  `addQuan` int(11) NOT NULL DEFAULT '0' COMMENT 'Additional Quantity',
-  `curQuan` int(11) NOT NULL DEFAULT '0' COMMENT 'Current Quantity',
-  PRIMARY KEY (`id`)
+  `priceBeforeTax` double NOT NULL default '0',
+  `tax` double NOT NULL default '0',
+  `price_after_tax` double NOT NULL default '0',
+  `addQuan` int(11) NOT NULL default '0' COMMENT 'Additional Quantity',
+  `curQuan` int(11) NOT NULL default '0' COMMENT 'Current Quantity',
+  PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
@@ -90,14 +166,14 @@ CREATE TABLE IF NOT EXISTS `tbl_goods` (
 -- Table structure for table `tbl_invoice`
 --
 
-CREATE TABLE IF NOT EXISTS `tbl_invoice` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tbl_invoice` (
+  `id` int(11) NOT NULL auto_increment,
   `created_date` date NOT NULL,
   `customer_id` int(11) NOT NULL,
-  `total_before_tax` double NOT NULL DEFAULT '0',
-  `tax_total` double NOT NULL DEFAULT '0',
-  `total_after_tax` double NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
+  `total_before_tax` double NOT NULL default '0',
+  `tax_total` double NOT NULL default '0',
+  `total_after_tax` double NOT NULL default '0',
+  PRIMARY KEY  (`id`),
   KEY `customer_id` (`customer_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
