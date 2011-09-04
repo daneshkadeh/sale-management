f850e58b6f12e6db61aed2c02393ebea0fa7ec88
hoanglt705 <hoanglt705@b168cc76-a07e-71fc-40dc-6a1c1b62a871>
2011-09-04 15:59:53 +0000

    _add BaseCredential.java, BasePrincipal.java and make SalesPrincipal, UsernameCredential extend them

--- trunk/ssm/ssm-core/src/main/java/com/hbsoft/ssm/security/SalesPrincipal.java
+++ trunk/ssm/ssm-core/src/main/java/com/hbsoft/ssm/security/SalesPrincipal.java
@@ -1,46 +1,9 @@
 package com.hbsoft.ssm.security;
 
-import java.security.Principal;
+public class SalesPrincipal extends BasePrincipal {
 
-public class SalesPrincipal implements Principal {
-    private Integer id;
-    private String name;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public SalesPrincipal(Integer id, String name) {
-        super();
-        this.id = id;
-        this.name = name;
-    }
-
-    public String getName() {
-		// TODO Auto-generated method stub
-		return this.name;
+	public SalesPrincipal(String name) {
+		super(name);
 	}
-
-	@Override
-    public boolean equals(Object obj) {
-        // TODO Auto-generated method stub
-        if (!(obj instanceof SalesPrincipal)) {
-            return false;
-        }
-        SalesPrincipal other = (SalesPrincipal) obj;
-        return getName().equals(other.getName()) && getId().equals(other.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return getName().hashCode() * 27 + getId().hashCode() * 27;
-    }
+   
 }
