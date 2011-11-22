070cdfe1a87093cc7e4763a86a4081cb5bffed32
hoanglt705 <hoanglt705@b168cc76-a07e-71fc-40dc-6a1c1b62a871>
2011-11-22 15:36:13 +0000

    __add view/detail/security

---
+++ ssm/ssm-gui/src/main/java/com/s3s/ssm/view/detail/security/EditRoleView.java
@@ -0,0 +1,25 @@
+package com.s3s.ssm.view.detail.security;
+
+import com.s3s.ssm.entity.security.User;
+import com.s3s.ssm.model.DetailDataModel;
+import com.s3s.ssm.model.DetailDataModel.FieldTypeEnum;
+import com.s3s.ssm.view.AbstractDetailView;
+
+public class EditRoleView extends AbstractDetailView<User> {
+    private static final long serialVersionUID = 1L;
+
+    public EditRoleView(User entity) {
+        super(entity);
+    }
+
+    @Override
+    public void initialPresentationView(DetailDataModel detailDataModel, User entity) {
+        detailDataModel.addAttribute("username", FieldTypeEnum.TEXTBOX);
+        detailDataModel.addAttribute("password", FieldTypeEnum.TEXTBOX);
+        detailDataModel.addAttribute("isAccountNonExpired", FieldTypeEnum.TEXTBOX);
+        detailDataModel.addAttribute("isAccountNonLocked", FieldTypeEnum.TEXTBOX);
+        detailDataModel.addAttribute("isCredentialsNonExpired", FieldTypeEnum.TEXTBOX);
+        detailDataModel.addAttribute("isEnabled", FieldTypeEnum.TEXTBOX);
+    }
+
+}
