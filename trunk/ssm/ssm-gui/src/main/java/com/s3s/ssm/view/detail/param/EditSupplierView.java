6cadb5321daa4f7ef6ae2b336760b2b14b9794a1
congbangmm83@gmail.com <congbangmm83@gmail.com@b168cc76-a07e-71fc-40dc-6a1c1b62a871>
2011-11-27 06:34:49 +0000

    __Update AbstractDetailView, using BeanWrapper to update sub-entity in object

--- trunk/ssm/ssm-gui/src/main/java/com/s3s/ssm/view/detail/param/EditSupplierView.java
+++ trunk/ssm/ssm-gui/src/main/java/com/s3s/ssm/view/detail/param/EditSupplierView.java
@@ -1,11 +1,18 @@
 package com.s3s.ssm.view.detail.param;
 
+import org.apache.commons.lang.StringUtils;
+
+import com.s3s.ssm.entity.contact.Bank;
+import com.s3s.ssm.entity.contact.BankAccount;
 import com.s3s.ssm.entity.param.Supplier;
 import com.s3s.ssm.model.DetailDataModel;
 import com.s3s.ssm.model.DetailDataModel.FieldTypeEnum;
+import com.s3s.ssm.model.ReferenceDataModel;
 import com.s3s.ssm.view.AbstractDetailView;
 
 public class EditSupplierView extends AbstractDetailView<Supplier> {
+
+    private static final String REF_BANK = "REF_BANK";
 
     public EditSupplierView(Supplier entity) {
         super(entity);
@@ -19,10 +26,39 @@ public class EditSupplierView extends AbstractDetailView<Supplier> {
         detailDataModel.addAttribute("phoneNumber", FieldTypeEnum.TEXTBOX);
         detailDataModel.addAttribute("fixPhoneNumber", FieldTypeEnum.TEXTBOX);
         detailDataModel.addAttribute("faxNumber", FieldTypeEnum.TEXTBOX);
-        // detailDataModel.addAttribute("bankAccount.bank", FieldTypeEnum.DROPDOWN);
-        // detailDataModel.addAttribute("bankAccount.accountNumber", FieldTypeEnum.TEXTBOX);
-        // detailDataModel.addAttribute("bankAccount.accountName", FieldTypeEnum.TEXTBOX);
+        detailDataModel.addAttribute("bankAccount.bank", FieldTypeEnum.DROPDOWN).referenceDataId(REF_BANK);
+        detailDataModel.addAttribute("bankAccount.accountNumber", FieldTypeEnum.TEXTBOX);
+        detailDataModel.addAttribute("bankAccount.accountName", FieldTypeEnum.TEXTBOX);
         detailDataModel.addAttribute("note", FieldTypeEnum.TEXTBOX);
+    }
+
+    @Override
+    protected void loadForCreate(Supplier entity) {
+        super.loadForCreate(entity);
+        entity.setBankAccount(new BankAccount());
+    }
+
+    @Override
+    protected void loadForEdit(Supplier entity) {
+        super.loadForEdit(entity);
+        if (entity.getBankAccount() == null) {
+            entity.setBankAccount(new BankAccount());
+        }
+    }
+
+    @Override
+    protected void saveOrUpdate(Supplier entity) {
+        // TODO: Must save bank account before supplier. How to avoid it?
+        if (StringUtils.isNotEmpty(entity.getBankAccount().getAccountNumber())) {
+            getDaoHelper().getDao(BankAccount.class).saveOrUpdate(entity.getBankAccount());
+        }
+        super.saveOrUpdate(entity);
+    }
+
+    @Override
+    protected void setReferenceDataModel(ReferenceDataModel refDataModel, Supplier entity) {
+        super.setReferenceDataModel(refDataModel, entity);
+        refDataModel.putRefDataList(REF_BANK, getDaoHelper().getDao(Bank.class).findAll(), null);
     }
 
 }
