b1857e50dec916ecbb826475b35ec0033869ee5c
congbangmm83@gmail.com <congbangmm83@gmail.com@b168cc76-a07e-71fc-40dc-6a1c1b62a871>
2011-12-26 15:19:26 +0000

    __prepare test folder

---
+++ trunk/ssm/ssm-config/src/main/java/com/s3s/ssm/entity/config/SCurrency.java
@@ -0,0 +1,51 @@
+package com.s3s.ssm.entity.config;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+
+import com.s3s.ssm.entity.AbstractCodeOLObject;
+
+@Entity
+@Table(name = "s_currency")
+public class SCurrency extends AbstractCodeOLObject {
+	private String name;
+	private String symbol;
+	private Boolean isActive = true;
+	
+	@Column(name = "name", length = 128)
+    @NotNull
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Column(name = "symbol", length = 10)
+	public String getSymbol() {
+		return symbol;
+	}
+
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+	@Column(name = "active")
+	public Boolean getIsActive() {
+		return isActive;
+	}
+
+	public void setIsActive(Boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+	
+	
+
+}
