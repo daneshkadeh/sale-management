b169375bddcc1836a31fb37da33544097694edcf
congbangmm83@gmail.com <congbangmm83@gmail.com@b168cc76-a07e-71fc-40dc-6a1c1b62a871>
2011-12-28 05:26:07 +0000

    __Fix bug CHECK_BOX + add screen PaymentType

--- trunk/ssm/ssm-finance/src/main/java/com/s3s/ssm/view/detail/finance/EditPaymentTypeView.java
+++ trunk/ssm/ssm-finance/src/main/java/com/s3s/ssm/view/detail/finance/EditPaymentTypeView.java
@@ -1,20 +1,34 @@
 package com.s3s.ssm.view.detail.finance;
 
+import java.util.Arrays;
+
+import com.s3s.ssm.entity.finance.PaymentContentType;
 import com.s3s.ssm.entity.finance.PaymentType;
 import com.s3s.ssm.model.DetailDataModel;
+import com.s3s.ssm.model.DetailDataModel.FieldTypeEnum;
+import com.s3s.ssm.model.ReferenceDataModel;
 import com.s3s.ssm.view.AbstractDetailView;
 
 public class EditPaymentTypeView extends AbstractDetailView<PaymentType> {
 
+    private static final String REF_CONTENT_TYPE = "contentType";
+
     public EditPaymentTypeView(PaymentType entity) {
         super(entity);
-        // TODO Auto-generated constructor stub
     }
 
     @Override
     public void initialPresentationView(DetailDataModel detailDataModel, PaymentType entity) {
-        // TODO Auto-generated method stub
+        detailDataModel.addAttribute("code", FieldTypeEnum.TEXTBOX);
+        detailDataModel.addAttribute("name", FieldTypeEnum.TEXTBOX);
+        detailDataModel.addAttribute("contentType", FieldTypeEnum.DROPDOWN).referenceDataId(REF_CONTENT_TYPE);
+        detailDataModel.addAttribute("isReceived", FieldTypeEnum.CHECKBOX);
+    }
 
+    @Override
+    protected void setReferenceDataModel(ReferenceDataModel refDataModel, PaymentType entity) {
+        super.setReferenceDataModel(refDataModel, entity);
+        refDataModel.putRefDataList(REF_CONTENT_TYPE, Arrays.asList(PaymentContentType.values()), null);
     }
 
 }
