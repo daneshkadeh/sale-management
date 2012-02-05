f4764208327beeec4882d85b40704147ac7a24ff
hoanglt705 <hoanglt705@b168cc76-a07e-71fc-40dc-6a1c1b62a871>
2012-02-05 12:47:57 +0000

    __Add ShipPrice into Shipment module

---
+++ ssm/ssm-shipment/src/main/java/com/s3s/ssm/view/detail/shipment/EditShipPriceView.java
@@ -0,0 +1,46 @@
+/*
+ * EditShipmentPrice
+ * 
+ * Project: SSM
+ * 
+ * Copyright 2010 by HBASoft
+ * Rue de la Berg�re 7, 1217 Meyrin
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information
+ * of HBASoft. ("Confidential Information"). You
+ * shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license
+ * agreements you entered into with HBASoft.
+ */
+
+package com.s3s.ssm.view.detail.shipment;
+
+import com.s3s.ssm.entity.shipment.ShipPrice;
+import com.s3s.ssm.model.DetailDataModel;
+import com.s3s.ssm.model.DetailDataModel.FieldTypeEnum;
+import com.s3s.ssm.view.AbstractDetailView;
+
+/**
+ * @author Le Thanh Hoang
+ * 
+ */
+public class EditShipPriceView extends AbstractDetailView<ShipPrice> {
+
+    /**
+     * @param entity
+     */
+    public EditShipPriceView(ShipPrice entity) {
+        super(entity);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void initialPresentationView(DetailDataModel detailDataModel, ShipPrice entity) {
+        detailDataModel.addAttribute("code", FieldTypeEnum.TEXTBOX).setMandatory(true);
+        detailDataModel.addAttribute("name", FieldTypeEnum.TEXTBOX).setMandatory(true);
+    }
+
+}
