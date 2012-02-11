88ec7dd0ec0e3e2aae8318e496debd39c56d3772
phanhongphucit <phanhongphucit@b168cc76-a07e-71fc-40dc-6a1c1b62a871>
2012-02-11 16:51:07 +0000

    Add missing file

---
+++ ssm/ssm-contact/src/main/java/com/s3s/ssm/view/detail/contact/EditMultiSupplierViewTest.java
@@ -0,0 +1,42 @@
+/*
+ * EditMultiSupplierViewTest
+ * 
+ * Project: SSM
+ * 
+ * Copyright 2010 by HBASoft
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information
+ * of HBASoft. ("Confidential Information"). You
+ * shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license
+ * agreements you entered into with HBASoft.
+ */
+
+package com.s3s.ssm.view.detail.contact;
+
+import com.s3s.ssm.entity.contact.Supplier;
+import com.s3s.ssm.view.AbstractMultiEditView;
+import com.s3s.ssm.view.TreeNodeWithView;
+
+/**
+ * @author Phan Hong Phuc
+ * 
+ */
+public class EditMultiSupplierViewTest extends AbstractMultiEditView<Supplier> {
+    private static final long serialVersionUID = -427718510574119477L;
+
+    public EditMultiSupplierViewTest(Supplier entity) {
+        super(entity);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void constructTreeView(TreeNodeWithView root, Supplier entity) {
+        TreeNodeWithView node = new TreeNodeWithView("Supplier", new EditSupplierView(entity));
+        root.add(node);
+    }
+
+}
