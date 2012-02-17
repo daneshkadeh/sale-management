f68a9a2ce1ca6bbebf13c7b3af8a9a1e6be8b38e
phanhongphucit <phanhongphucit@b168cc76-a07e-71fc-40dc-6a1c1b62a871>
2012-02-17 19:13:05 +0000

    Implement check dirty for Edit view. Remove testing validation in Supplier.

---
+++ ssm/ssm-core/src/main/java/com/s3s/ssm/util/ImageConstants.java
@@ -0,0 +1,27 @@
+/*
+ * ImageConstants
+ * 
+ * Project: SSM
+ * 
+ * Copyright 2010 by HBASoft
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information
+ * of HBASoft. ("Confidential Information"). You
+ * shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license
+ * agreements you entered into with HBASoft.
+ */
+
+package com.s3s.ssm.util;
+
+/**
+ * @author Phan Hong Phuc
+ *
+ */
+public class ImageConstants {
+    public static final String USER_ICON = "/icons/user.png";
+    public static final String ERROR_ICON = "/icons/errorIcon.gif";
+    public static final String INFO_ICON = "/icons/infoIcon.png";
+    public static final String WARNING_ICON = "/icons/warningIcon.png";
+}
