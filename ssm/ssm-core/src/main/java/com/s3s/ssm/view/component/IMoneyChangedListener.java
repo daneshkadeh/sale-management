e0229f88e656b02517f19e6066b74c1e092f0b98
phanhongphucit <phanhongphucit@b168cc76-a07e-71fc-40dc-6a1c1b62a871>
2012-03-22 19:35:48 +0000

    Add action listener to MoneyComponent to observe the money change.

---
+++ ssm/ssm-core/src/main/java/com/s3s/ssm/view/component/IMoneyChangedListener.java
@@ -0,0 +1,33 @@
+/*
+ * IMoneyChanged
+ * 
+ * Project: SSM
+ * 
+ * Copyright 2010 by HBASoft
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information
+ * of HBASoft. ("Confidential Information"). You
+ * shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license
+ * agreements you entered into with HBASoft.
+ */
+
+package com.s3s.ssm.view.component;
+
+import java.util.EventListener;
+
+import javax.swing.event.ChangeEvent;
+
+/**
+ * The listener interface for receiving the value or currency code changed.
+ * 
+ * @author Phan Hong Phuc
+ * @since Mar 23, 2012
+ */
+public interface IMoneyChangedListener extends EventListener {
+    /**
+     * Invoke when the value or currency code changed.
+     */
+    public void doMoneyChanged(ChangeEvent e);
+}
