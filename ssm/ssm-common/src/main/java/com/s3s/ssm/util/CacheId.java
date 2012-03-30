c175522bf48df5966eed025594ed3843f6f34a9f
hoanglt705 <hoanglt705@b168cc76-a07e-71fc-40dc-6a1c1b62a871>
2012-03-30 03:29:46 +0000

    __update cache id

--- ssm/ssm-common/src/main/java/com/s3s/ssm/util/CacheId.java
+++ ssm/ssm-common/src/main/java/com/s3s/ssm/util/CacheId.java
@@ -2,6 +2,10 @@ package com.s3s.ssm.util;
 
 public interface CacheId {
     public static String REF_LIST_PARTNER = "referenceListPartner";
+    public static String REF_LIST_OPERATOR = "referenceListOperator";
     public static String REF_LIST_PRODUCT = "referenceListProduct";
     public static String REF_LIST_CURRENCY = "referenceListCurrency";
+    public static String REF_LIST_BANK = "referenceListBank";
+    public static String REF_LIST_PAYMENT_CONTENT = "referenceListPaymentContent";
+    public static String REF_LIST_RECEIPT_CONTENT = "referenceListReceiptContent";
 }
