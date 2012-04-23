359a93a7211a30d0b5836d689c92e89890798b66
congbangmm83 <congbangmm83@gmail.com>
2012-04-23 00:17:31 +0000

    __ add search panel for views which can have a lot of entities.

--- ssm/ssm-catalog-view/src/main/java/com/s3s/ssm/view/list/param/ListGoodsView.java
+++ ssm/ssm-catalog-view/src/main/java/com/s3s/ssm/view/list/param/ListGoodsView.java
@@ -14,14 +14,37 @@
  */
 package com.s3s.ssm.view.list.param;
 
+import java.util.List;
+
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import net.miginfocom.swing.MigLayout;
+
+import org.apache.commons.lang.StringUtils;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Restrictions;
+
 import com.s3s.ssm.entity.catalog.Goods;
+import com.s3s.ssm.entity.catalog.Manufacturer;
+import com.s3s.ssm.entity.catalog.ProductType;
+import com.s3s.ssm.util.i18n.ControlConfigUtils;
 import com.s3s.ssm.view.detail.param.EditGoodsView;
 import com.s3s.ssm.view.edit.AbstractEditView;
-import com.s3s.ssm.view.list.AbstractListView;
+import com.s3s.ssm.view.list.AbstractSearchListView;
 import com.s3s.ssm.view.list.ListDataModel;
 import com.s3s.ssm.view.list.ListDataModel.ListRendererType;
 
-public class ListGoodsView extends AbstractListView<Goods> {
+public class ListGoodsView extends AbstractSearchListView<Goods> {
+
+    private JTextField codeField;
+    private JTextField nameField;
+    private JTextField modelField;
+    private JComboBox<ProductType> typeField;
+    private JComboBox<Manufacturer> manufacturerField; // TODO: change to search component if there are a lot.
 
     @Override
     protected void initialPresentationView(ListDataModel listDataModel) {
@@ -37,6 +60,66 @@ public class ListGoodsView extends AbstractListView<Goods> {
     @Override
     protected Class<? extends AbstractEditView<Goods>> getEditViewClass() {
         return EditGoodsView.class;
+    }
+
+    @Override
+    protected DetachedCriteria getCriteriaForView() {
+        DetachedCriteria dc = super.getCriteriaForView();
+        if (StringUtils.isNotBlank(codeField.getText())) {
+            dc.add(Restrictions.ilike("code", codeField.getText(), MatchMode.ANYWHERE));
+        }
+        if (StringUtils.isNotBlank(nameField.getText())) {
+            dc.add(Restrictions.ilike("name", nameField.getText(), MatchMode.ANYWHERE));
+        }
+        if (StringUtils.isNotBlank(modelField.getText())) {
+            dc.add(Restrictions.ilike("model", modelField.getText(), MatchMode.ANYWHERE));
+        }
+        if (typeField.getSelectedItem() != null) {
+            dc.add(Restrictions.eq("type", typeField.getSelectedItem()));
+        }
+        if (manufacturerField.getSelectedItem() != null) {
+            dc.add(Restrictions.eq("manufacturer", manufacturerField.getSelectedItem()));
+        }
+
+        return dc;
+    }
+
+    @Override
+    protected JPanel createSearchPanel() {
+        codeField = new JTextField();
+        nameField = new JTextField();
+        modelField = new JTextField();
+        List<ProductType> productTypes = getDaoHelper().getDao(ProductType.class).findAll();
+        productTypes.add(0, null);
+        typeField = new JComboBox<ProductType>(productTypes.toArray(new ProductType[productTypes.size()]));
+
+        List<Manufacturer> manufacturers = getDaoHelper().getDao(Manufacturer.class).findAll();
+        manufacturers.add(0, null);
+        manufacturerField = new JComboBox<Manufacturer>(manufacturers.toArray(new Manufacturer[manufacturers.size()]));
+
+        JPanel panel = new JPanel(new MigLayout("ins 0, fill", "grow"));
+        panel.add(new JLabel(ControlConfigUtils.getString("label.Goods.code")), "right");
+        panel.add(codeField, "grow");
+        panel.add(new JLabel(ControlConfigUtils.getString("label.Goods.name")), "right");
+        panel.add(nameField, "grow");
+        panel.add(new JLabel(ControlConfigUtils.getString("label.Goods.model")), "right");
+        panel.add(modelField, "grow, wrap");
+
+        panel.add(new JLabel(ControlConfigUtils.getString("label.Goods.type")), "right");
+        panel.add(typeField, "grow");
+        panel.add(new JLabel(ControlConfigUtils.getString("label.Goods.manufacturer")), "right");
+        panel.add(manufacturerField, "grow");
+
+        return panel;
+    }
+
+    @Override
+    protected void clearCriteria() {
+        codeField.setText(null);
+        nameField.setText(null);
+        modelField.setText(null);
+        typeField.setSelectedItem(null);
+        manufacturerField.setSelectedItem(null);
     }
 
 }
--- ssm/ssm-sales-view/src/main/java/com/s3s/ssm/view/list/sales/ListSalesContractView.java
+++ ssm/ssm-sales-view/src/main/java/com/s3s/ssm/view/list/sales/ListSalesContractView.java
@@ -14,14 +14,95 @@
  */
 package com.s3s.ssm.view.list.sales;
 
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import net.miginfocom.swing.MigLayout;
+
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Restrictions;
+import org.jdesktop.swingx.JXDatePicker;
+
 import com.s3s.ssm.entity.sales.SalesContract;
+import com.s3s.ssm.entity.sales.SalesContractStatus;
+import com.s3s.ssm.util.i18n.ControlConfigUtils;
 import com.s3s.ssm.view.detail.sales.EditSalesContractView;
 import com.s3s.ssm.view.edit.AbstractEditView;
-import com.s3s.ssm.view.list.AbstractListView;
+import com.s3s.ssm.view.list.AbstractSearchListView;
 import com.s3s.ssm.view.list.ListDataModel;
 import com.s3s.ssm.view.list.ListDataModel.ListRendererType;
 
-public class ListSalesContractView extends AbstractListView<SalesContract> {
+public class ListSalesContractView extends AbstractSearchListView<SalesContract> {
+
+    private JTextField contractCode;
+    private JTextField supplierCode; // TODO: will be changed to searchComponent
+    private JComboBox<SalesContractStatus> contractStatus;
+    private JXDatePicker startDateContract;
+    private JXDatePicker endDateContract;
+
+    @Override
+    protected JPanel createSearchPanel() {
+        contractCode = new JTextField();
+        supplierCode = new JTextField();
+        // TODO: the null value should have a label. Should use an enum util to translate from enum to i18n.
+        SalesContractStatus[] fullStatus = (SalesContractStatus[]) ArrayUtils
+                .add(SalesContractStatus.values(), 0, null);
+        contractStatus = new JComboBox<SalesContractStatus>(fullStatus);
+        startDateContract = new JXDatePicker();
+        endDateContract = new JXDatePicker();
+
+        JPanel panel = new JPanel(new MigLayout("ins 0, fill", "grow"));
+        panel.add(new JLabel(ControlConfigUtils.getString("label.SalesContract.code")), "right");
+        panel.add(contractCode, "grow");
+
+        panel.add(new JLabel(ControlConfigUtils.getString("label.SalesContract.dateContract") + " >"), "right");
+        panel.add(startDateContract, "grow");
+        panel.add(new JLabel(ControlConfigUtils.getString("label.SalesContract.dateContract") + " <"), "right");
+        panel.add(endDateContract, "grow, wrap");
+
+        panel.add(new JLabel(ControlConfigUtils.getString("label.SalesContract.supplier")), "right");
+        panel.add(supplierCode, "grow");
+        panel.add(new JLabel(ControlConfigUtils.getString("label.SalesContract.status")), "right");
+        panel.add(contractStatus, "grow, wrap");
+
+        return panel;
+    }
+
+    @Override
+    protected void clearCriteria() {
+        contractCode.setText(null);
+        contractStatus.setSelectedItem(null);
+        startDateContract.setDate(null);
+        endDateContract.setDate(null);
+        supplierCode.setText(null);
+
+    }
+
+    @Override
+    protected DetachedCriteria getCriteriaForView() {
+        DetachedCriteria criteria = super.getCriteriaForView();
+        if (StringUtils.isNotBlank(contractCode.getText())) {
+            criteria.add(Restrictions.ilike("code", contractCode.getText(), MatchMode.ANYWHERE));
+        }
+        if (contractStatus.getSelectedItem() != null) {
+            criteria.add(Restrictions.eq("status", contractStatus.getSelectedItem()));
+        }
+        if (startDateContract.getDate() != null) {
+            criteria.add(Restrictions.ge("dateContract", startDateContract.getDate()));
+        }
+        if (endDateContract.getDate() != null) {
+            criteria.add(Restrictions.le("dateContract", endDateContract.getDate()));
+        }
+        if (StringUtils.isNotBlank(supplierCode.getText())) {
+            criteria.createCriteria("supplier").add(Restrictions.eq("code", supplierCode.getText()));
+        }
+        return criteria;
+    }
 
     @Override
     protected void initialPresentationView(ListDataModel listDataModel) {
