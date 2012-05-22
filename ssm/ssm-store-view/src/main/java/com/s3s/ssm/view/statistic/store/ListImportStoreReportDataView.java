49098c3530de32c88c769dbd7029f439497da813
phanhongphucit <phanhongphucit@b168cc76-a07e-71fc-40dc-6a1c1b62a871>
2012-05-22 15:06:05 +0000

    Báo cáo nhập hàng report

--- ssm/ssm-store-view/src/main/java/com/s3s/ssm/view/statistic/store/ListImportStoreReportDataView.java
+++ ssm/ssm-store-view/src/main/java/com/s3s/ssm/view/statistic/store/ListImportStoreReportDataView.java
@@ -16,14 +16,16 @@
 
 package com.s3s.ssm.view.statistic.store;
 
-import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 
 import net.miginfocom.swing.MigLayout;
 
@@ -34,6 +36,7 @@ import com.s3s.ssm.entity.catalog.Product;
 import com.s3s.ssm.entity.store.Store;
 import com.s3s.ssm.interfaces.catalog.ICatalogService;
 import com.s3s.ssm.interfaces.store.IStoreService;
+import com.s3s.ssm.util.IziJasperReportUtils;
 import com.s3s.ssm.util.i18n.ControlConfigUtils;
 import com.s3s.ssm.util.view.UIConstants;
 import com.s3s.ssm.view.list.AListDataView;
@@ -75,11 +78,10 @@ public class ListImportStoreReportDataView extends AListDataView<GroupDetailImpo
         String salesContractCode = salesContactComp.getText();
         Store selectedStore = (Store) cbStore.getSelectedItem();
         Product selectedProduct = (Product) cbProduct.getSelectedItem();
-        if (selectedStore == null || selectedProduct == null) {
-            return Collections.EMPTY_LIST;
-        }
-        return serviceProvider.getService(IStoreService.class).statisticImportStoreData(salesContractCode,
-                selectedStore.getCode(), selectedProduct.getCode(), fromDate, toDate);
+        // TODO Phuc pass firstIndex and maxResult to paging
+        return serviceProvider.getService(IStoreService.class).getStatisticImportStoreData(salesContractCode,
+                selectedStore == null ? "" : selectedStore.getCode(),
+                selectedProduct == null ? "" : selectedProduct.getCode(), fromDate, toDate);
     }
 
     /**
@@ -87,8 +89,14 @@ public class ListImportStoreReportDataView extends AListDataView<GroupDetailImpo
      */
     @Override
     protected int calculateTotalPages() {
-        // TODO Phuc
-        return 1;
+        Date fromDate = fromDateComp.getDate();
+        Date toDate = toDateComp.getDate();
+        String salesContractCode = salesContactComp.getText();
+        Store selectedStore = (Store) cbStore.getSelectedItem();
+        Product selectedProduct = (Product) cbProduct.getSelectedItem();
+        return serviceProvider.getService(IStoreService.class).getStatisticImportStoreDataCount(salesContractCode,
+                selectedStore == null ? "" : selectedStore.getCode(),
+                selectedProduct == null ? "" : selectedProduct.getCode(), fromDate, toDate);
     }
 
     /**
@@ -135,6 +143,17 @@ public class ListImportStoreReportDataView extends AListDataView<GroupDetailImpo
     @Override
     protected void performPrintAction() {
         // TODO Phuc
+        List<GroupDetailImportData> data = loadData(-1, -1);
+        Map<String, Object> params = new HashMap<String, Object>();
+        String productNameFilter = cbProduct.getSelectedItem() == null ? "" : ((Product) cbProduct.getSelectedItem())
+                .getName();
+        String storeNameFilter = cbStore.getSelectedItem() == null ? "" : ((Store) cbStore.getSelectedItem()).getName();
+        params.put("PRODUCT_NAME", productNameFilter);
+        params.put("STORE", storeNameFilter);
+        params.put("CONTRACT_NUM", salesContactComp.getText());
+        params.put("FROM_DATE", fromDateComp.getDate());
+        params.put("TO_DATE", toDateComp.getDate());
+        IziJasperReportUtils.showReportDialog(SwingUtilities.getWindowAncestor(this), "/reports/importStore.jasper",
+                params, data);
     }
-
 }
